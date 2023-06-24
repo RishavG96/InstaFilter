c64bb967753d83386d83b847b3467ca59129ac7e
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-24 10:11:40 +0530

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Rishav Gupta on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
